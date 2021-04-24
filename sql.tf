11066d6d310bfb6c9a468122930f89f222780179
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 23:35:29 -0400

    1

--- sql.tf
+++ sql.tf
@@ -1,9 +1,3 @@
- connection_string {
-    name  = "Database"
-    type  = "SQLAzure"
-    value = "Server=tcp:azurerm_sql_server.sqldb.fully_qualified_domain_name Database=azurerm_sql_database.db.name;User ID=azurerm_sql_server.sqldb.administrator_login;Password=azurerm_sql_server.sqldb.administrator_login_password;Trusted_Connection=False;Encrypt=True;"
-  }
-
 resource "azurerm_sql_server" "sqldb" {
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.rg.name
